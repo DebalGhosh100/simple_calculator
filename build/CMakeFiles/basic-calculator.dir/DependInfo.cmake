452a702f1d0671ba56cf72c4f8f8d504e72767f2
DebalGhosh100 <debalghosh100@gmail.com>
2025-08-11 18:12:47 +0530

    Updated build logs

---
+++ build/CMakeFiles/basic-calculator.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/debalghosh/simple_calculator/simple_calculator/main/calculator/calculator.cpp" "/home/debalghosh/simple_calculator/simple_calculator/build/CMakeFiles/basic-calculator.dir/home/debalghosh/simple_calculator/simple_calculator/main/calculator/calculator.cpp.o"
+  "/home/debalghosh/simple_calculator/simple_calculator/main/calculator/operations/operations.cpp" "/home/debalghosh/simple_calculator/simple_calculator/build/CMakeFiles/basic-calculator.dir/home/debalghosh/simple_calculator/simple_calculator/main/calculator/operations/operations.cpp.o"
+  "/home/debalghosh/simple_calculator/simple_calculator/main/main_calc.cpp" "/home/debalghosh/simple_calculator/simple_calculator/build/CMakeFiles/basic-calculator.dir/home/debalghosh/simple_calculator/simple_calculator/main/main_calc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../main/calculator/operations"
+  "../main/calculator"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
